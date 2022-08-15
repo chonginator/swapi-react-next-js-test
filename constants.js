da5676c3b74f9361a171e439f2f7a64980344912
Ben Chong <benchong1999@yahoo.com.au>
2022-08-15 13:48:59 +1000

    Added a list of Star Wars films to the home page from SWAPI

---
+++ constants.js
@@ -0,0 +1,1 @@
+export const baseURL = 'https://www.swapi.tech/api/films/';
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,31 @@
+import { baseURL } from '../constants';
+import { useState, useEffect } from 'react';
+
 const Home = () => {
+  const [filmData, setFilmData] = useState([])
+
+  // Fetch list of films from SWAPI
+  useEffect(() => {
+    fetch(baseURL)
+      .then(res => res.json())
+      .then(data => setFilmData(data.result))
+      .catch(error => console.error(error))
+  }, [])
+
+  const films = filmData.map(
+    ({ properties, uid }) => {
+      return (
+        <article key={uid}>
+          <h2>Star Wars: {properties.title}</h2>
+        </article>
+      )
+    }
+  )
   return (
-    <></>
+    <main>
+      <h1>Star Wars Films</h1>
+      {films}
+    </main>
   )
 }
 
