d214ec59ab9aa372e044e4e36cdd4b51a26ffc14
Ben Chong <benchong1999@yahoo.com.au>
2022-08-18 16:52:40 +1000

    Attempt to fix rate limiting error in resources.js

--- utils/resources.js
+++ utils/resources.js
@@ -1,9 +1,22 @@
 export async function getResources(URLs) {
     const requests = URLs.map(url => fetch(url));
-    const resourcesRes = await Promise.all(requests);
+    const resourcesRes = await Promise.allSettled(requests);
     const resourcesJSON = await Promise.all(
-        resourcesRes.map(res => res.json())
+        resourcesRes
+            .filter(outcome => outcome.status === 'fulfilled')
+            .map(outcome => {
+                try {
+                    return outcome.value.json()
+                } catch(e) {
+                    console.error(e)
+                }
+            })
     );
+    // const resourcesJSON = await Promise.all(
+    //     resourcesRes.map(res => res.json())
+    // );
+    // console.info(resourcesRes);
+    console.info(resourcesJSON);
     const resourcesData = await Promise.all(
         resourcesJSON.map(json => json.result)
     );
